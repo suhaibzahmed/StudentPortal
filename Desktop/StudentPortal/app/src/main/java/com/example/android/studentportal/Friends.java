20915871e619a25f6b4e3a6aa51c2f296fd88608
suhaibzahmed <suhaib.z.ahmed@gmail.com>
2019-04-12 19:32:17 +0530

    Updated project code

---
+++ Desktop/StudentPortal/app/src/main/java/com/example/android/studentportal/Friends.java
@@ -0,0 +1,22 @@
+package com.example.android.studentportal;
+
+public class Friends
+{
+    public String date;
+
+    public Friends()
+    {
+    }
+
+    public Friends(String data) {
+        this.date = date;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
